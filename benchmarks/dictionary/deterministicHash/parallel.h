d285e54ce649af55a926af62c1a6dd49d142a4aa
mdole <dolematt@grinnell.edu>
2014-07-01 13:19:11 -0500

    added cmu dictionary benchmark

---
+++ benchmarks/dictionary/deterministicHash/parallel.h
@@ -0,0 +1,56 @@
+#define CILKP
+//#define OPENMP
+
+// cilkarts cilk++
+#if defined(CILK)
+#include <cilk.h>
+#define parallel_main cilk_main
+#define parallel_for cilk_for
+#define parallel_for_1 _Pragma("cilk_grainsize = 1") cilk_for
+#define parallel_for_256 _Pragma("cilk_grainsize = 256") cilk_for
+#define version_string "Compiled using cilkarts cilk++"
+
+// intel cilk+
+#elif defined(CILKP)
+#include <cilk/cilk.h>
+#define parallel_for cilk_for
+#define parallel_main main
+#define parallel_for_1 _Pragma("cilk grainsize = 1") cilk_for
+#define parallel_for_256 _Pragma("cilk grainsize = 256") cilk_for
+#define version_string "Compiled using intel cilk+"
+
+// openmp
+#elif defined(OPENMP)
+#include <omp.h>
+#define cilk_spawn
+#define cilk_sync
+#define parallel_main main
+#define parallel_for _Pragma("omp parallel for") for
+#define parallel_for_1 _Pragma("omp parallel for schedule (static,1)") for
+#define parallel_for_256 _Pragma("omp parallel for schedule (static,256)") for
+#define version_string "Compiled using OpenMP"
+
+// c++
+#else
+#define cilk_spawn
+#define cilk_sync
+#define parallel_main main
+#define parallel_for for
+#define parallel_for_1 for
+#define parallel_for_256 for
+#define cilk_for for
+#define version_string "Compiled using the default compiler"
+
+#endif
+
+#include <limits.h>
+
+#if defined(LONG)
+typedef long intT;
+typedef unsigned long uintT;
+#define INT_T_MAX LONG_MAX
+#else
+typedef int intT;
+typedef unsigned int uintT;
+#define INT_T_MAX INT_MAX
+#endif
